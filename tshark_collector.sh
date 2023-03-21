e82f13ad441a73497e1ef3450a5453c06e4a8899
matbord <matbord97@gmail.com>
2023-03-21 15:45:00 -0400

    interface updated

--- tshark_collector.sh
+++ tshark_collector.sh
@@ -6,6 +6,6 @@ elif [[ $# -eq 1 ]]; then
     FILENAME=tshark_log_$1.pcapng
 fi
 
-tshark -i lxdbr0 -a duration:10 --color -P -w $FILENAME
+tshark -i srs_spgw_sgi -a duration:10 --color -P -w $FILENAME
 # tshark -i tun_srsue -a duration:2750 --color -P -w $FILENAME
 # tshark -i srs_spgw_sgi -a duration:2750 --color -P -w $FILENAME
