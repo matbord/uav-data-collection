1bb37cdea1bbcf2725e7c7f14b38700d258dd755
matbord <matbord97@gmail.com>
2023-04-18 13:40:40 -0400

    client and server with fps

--- Client.py
+++ Client.py
@@ -4,6 +4,7 @@ import cv2
 import pickle
 import struct
 import imutils
+import time
 
 # Client socket
 # create an INET, STREAMing socket : 
@@ -17,12 +18,23 @@ client_socket.connect((host_ip,port))
 data = b""
 # Q: unsigned long long integer(8 bytes)
 payload_size = struct.calcsize("Q")
-
+# used to record the time when we processed last frame
+prev_frame_time = 0
+  
+# used to record the time at which we processed current frame
+new_frame_time = 0
+framesCount=0
 while True:
+    new_frame_time = time.time()   
     while len(data) < payload_size:
         packet = client_socket.recv(4*1024)
         if not packet: break
+        framesCount=framesCount+1
         data+=packet
+        if (new_frame_time-prev_frame_time>1.):
+            print(str(framesCount))
+            prev_frame_time = new_frame_time
+            framesCount=0
     packed_msg_size = data[:payload_size]
     data = data[payload_size:]
     msg_size = struct.unpack("Q",packed_msg_size)[0]
@@ -32,6 +44,7 @@ while True:
     data  = data[msg_size:]
     frame = pickle.loads(frame_data)
     cv2.imshow("Receiving...",frame)
+
     key = cv2.waitKey(10) 
     if key  == 13:
         break
--- Server.py
+++ Server.py
@@ -4,6 +4,8 @@ import cv2
 import pickle
 import struct
 import imutils
+import time
+
 
 # Server socket
 # create an INET, STREAMing socket
@@ -23,18 +25,22 @@ print('Socket bind complete')
 #It specifies the number of unaccepted connections that the system will allow before refusing new connections.
 server_socket.listen(5)
 print('Socket now listening')
-
+frame_rate = 10
+prev = 0
 while True:
     client_socket,addr = server_socket.accept()
     print('Connection from:',addr)
     if client_socket:
-        vid = cv2.VideoCapture(0 + CV_CAP_INTELPERC)
+        vid = cv2.VideoCapture(0)
         while(vid.isOpened()):
-            img,frame = vid.read()
-            a = pickle.dumps(frame)
-            message = struct.pack("Q",len(a))+a
-            client_socket.sendall(message)
-            cv2.imshow('Sending...',frame)
+            time_elapsed = time.time() - prev
+            if time_elapsed > 1./frame_rate:
+                prev = time.time()
+                img,frame = vid.read()
+                a = pickle.dumps(frame)
+                message = struct.pack("Q",len(a))+a
+                client_socket.sendall(message)
+                cv2.imshow('Sending...',frame)
             key = cv2.waitKey(10) 
             if key ==13:
                 client_socket.close()
