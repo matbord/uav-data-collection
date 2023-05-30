1ce46c218cc0873c7018dcf37ebac4dcfacb39fc
matbord <matbord97@gmail.com>
2023-05-30 15:32:39 -0400

    uploaded previous version due to mem problem

--- data-analysis.py
+++ data-analysis.py
@@ -1,77 +1,3 @@
-import argparse
-import csv  # try alternatively csvdict for better performance?
-import os
-import pyshark
-
-
-def main():
-    # Parse arguments
-    parser = argparse.ArgumentParser(
-        description='Read pcapng files, filter packets, and save them to csv files.'
-    )
-    parser.add_argument(
-        'pcapng_dir',
-        metavar='pcapng_dir',
-        type=str,
-        nargs='?',
-        default='data',
-        help='Relative path of the directory; supposed to accept an argument like "data".'
-    )
-    args = parser.parse_args()
-
-    # save absolute path for the directory where the pcapng files are saved
-    rel_dir = f"./{args.pcapng_dir}"
-
-    # read .pcapng files
-    for file in os.listdir(rel_dir):
-        if file.endswith('.pcapng'):
-            file_path = f"{rel_dir}/{file}"
-            file_path_no_ext, _ = os.path.splitext(file_path)
-            file_name = f"{args.pcapng_dir}/{file}"
-            capture = pyshark.FileCapture(file_name)
-
-            # check attributes
-            # see all attributes for mavlink_proto
-            # print(dir(capture[0].tcp.analysis))
-
-            # Iterate over the packets in the capture
-            data_map = {}
-            for packet in capture:
-                # Write the chosen variables in
-                # epoch time, tcp sequence number, pckt message id (command id),
-                # source ip/port, destination ip/port, packet size app layer
-                if 'mavlink_proto' in packet:
-                    # print duplicate packets
-                    # if packet.tcp.seq in data_map:
-                    #     print("Duplicate packet found: " + str(packet.tcp.seq))
-
-                    # packets with the same key are overwritten, so we only keep the last one
-                    data_map[packet.tcp.seq] = packet
-
-            # Create a new CSV file for writing
-            with open(file_path_no_ext + '.csv', 'w', newline='') as outfile:
-                # Create a CSV writer object
-                writer = csv.writer(outfile)
-                # Write headers
-                writer.writerow(["Timestamp", "SequenceNumber", "MavlinkCommand",
-                                "IPSourceHost", "TCPSourcePort", "IPDestHost",
-                                 "TCPDestPort", "PacketLength"])
-
-                pcktsvalues= data_map.values()
-                for packet in pcktsvalues:
-                    writer.writerow([packet.sniff_timestamp, packet.tcp.seq,
-                                    packet.mavlink_proto.get_field_by_showname(
-                                        'Payload'),
-                                    packet.ip.src_host, packet.tcp.srcport,
-                                    packet.ip.dst_host, packet.tcp.dstport,
-                                    packet.length])
-
-
-if __name__ == "__main__":
-    main()
-
-#OLD CODE WITHOUT REMOVING RETRANSMISSION
-
 # import argparse
 # import csv  # try alternatively csvdict for better performance?
 # import os
@@ -104,6 +30,24 @@ if __name__ == "__main__":
 #             file_name = f"{args.pcapng_dir}/{file}"
 #             capture = pyshark.FileCapture(file_name)
 
+#             # check attributes
+#             # see all attributes for mavlink_proto
+#             # print(dir(capture[0].tcp.analysis))
+
+#             # Iterate over the packets in the capture
+#             data_map = {}
+#             for packet in capture:
+#                 # Write the chosen variables in
+#                 # epoch time, tcp sequence number, pckt message id (command id),
+#                 # source ip/port, destination ip/port, packet size app layer
+#                 if 'mavlink_proto' in packet:
+#                     # print duplicate packets
+#                     # if packet.tcp.seq in data_map:
+#                     #     print("Duplicate packet found: " + str(packet.tcp.seq))
+
+#                     # packets with the same key are overwritten, so we only keep the last one
+#                     data_map[packet.tcp.seq] = packet
+
 #             # Create a new CSV file for writing
 #             with open(file_path_no_ext + '.csv', 'w', newline='') as outfile:
 #                 # Create a CSV writer object
@@ -111,25 +55,81 @@ if __name__ == "__main__":
 #                 # Write headers
 #                 writer.writerow(["Timestamp", "SequenceNumber", "MavlinkCommand",
 #                                 "IPSourceHost", "TCPSourcePort", "IPDestHost",
-#                                  "TCPDestPort", "PacketLength"]) 
+#                                  "TCPDestPort", "PacketLength"])
 
-#                 # check attributes
-#                 # see all attributes for mavlink_proto
-#                 # print(dir(capture[0].tcp.analysis))
+#                 pcktsvalues= data_map.values()
+#                 for packet in pcktsvalues:
+#                     writer.writerow([packet.sniff_timestamp, packet.tcp.seq,
+#                                     packet.mavlink_proto.get_field_by_showname(
+#                                         'Payload'),
+#                                     packet.ip.src_host, packet.tcp.srcport,
+#                                     packet.ip.dst_host, packet.tcp.dstport,
+#                                     packet.length])
 
-#                 # Iterate over the packets in the capture
-#                 for packet in capture:
-#                     # Write the chosen variables into the csv file. The variables are:
-#                     # epoch time, tcp sequence number, pckt message id (command id),
-#                     # source ip/port, destination ip/port, packet size app layer
-#                     if 'mavlink_proto' in packet:
-#                         writer.writerow([packet.sniff_timestamp, packet.tcp.seq,
-#                                         packet.mavlink_proto.get_field_by_showname(
-#                                             'Payload'),
-#                                         packet.ip.src_host, packet.tcp.srcport,
-#                                         packet.ip.dst_host, packet.tcp.dstport,
-#                                         packet.length])  
 
 # if __name__ == "__main__":
 #     main()
+
+#OLD CODE WITHOUT REMOVING RETRANSMISSION
+
+import argparse
+import csv  # try alternatively csvdict for better performance?
+import os
+import pyshark
+
+
+def main():
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='Read pcapng files, filter packets, and save them to csv files.'
+    )
+    parser.add_argument(
+        'pcapng_dir',
+        metavar='pcapng_dir',
+        type=str,
+        nargs='?',
+        default='data',
+        help='Relative path of the directory; supposed to accept an argument like "data".'
+    )
+    args = parser.parse_args()
+
+    # save absolute path for the directory where the pcapng files are saved
+    rel_dir = f"./{args.pcapng_dir}"
+
+    # read .pcapng files
+    for file in os.listdir(rel_dir):
+        if file.endswith('.pcapng'):
+            file_path = f"{rel_dir}/{file}"
+            file_path_no_ext, _ = os.path.splitext(file_path)
+            file_name = f"{args.pcapng_dir}/{file}"
+            capture = pyshark.FileCapture(file_name)
+
+            # Create a new CSV file for writing
+            with open(file_path_no_ext + '.csv', 'w', newline='') as outfile:
+                # Create a CSV writer object
+                writer = csv.writer(outfile)
+                # Write headers
+                writer.writerow(["Timestamp", "SequenceNumber", "MavlinkCommand",
+                                "IPSourceHost", "TCPSourcePort", "IPDestHost",
+                                 "TCPDestPort", "PacketLength"]) 
+
+                # check attributes
+                # see all attributes for mavlink_proto
+                # print(dir(capture[0].tcp.analysis))
+
+                # Iterate over the packets in the capture
+                for packet in capture:
+                    # Write the chosen variables into the csv file. The variables are:
+                    # epoch time, tcp sequence number, pckt message id (command id),
+                    # source ip/port, destination ip/port, packet size app layer
+                    if 'mavlink_proto' in packet:
+                        writer.writerow([packet.sniff_timestamp, packet.tcp.seq,
+                                        packet.mavlink_proto.get_field_by_showname(
+                                            'Payload'),
+                                        packet.ip.src_host, packet.tcp.srcport,
+                                        packet.ip.dst_host, packet.tcp.dstport,
+                                        packet.length])  
+
+if __name__ == "__main__":
+    main()
 
